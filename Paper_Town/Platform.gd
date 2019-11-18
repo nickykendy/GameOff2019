1f1c41213426de08bb36a6a8ca2c97037f491d33
nickykendy <nicky_kendy@hotmail.com>
2019-11-18 23:20:57 +0800

    you can grab a platform and move

--- Paper_Town/Platform.gd
+++ Paper_Town/Platform.gd
@@ -1,4 +1,18 @@
 extends KinematicBody2D
 
-func move(spd):
-	pass
+const G_SPD = 120
+
+var isGrabbed = false
+var velocity = Vector2()
+
+func grabbed(result):
+	isGrabbed = result
+
+func get_motion(motion):
+	velocity = motion
+
+func _physics_process(delta):
+	print(velocity)
+	print(isGrabbed)
+	if isGrabbed:
+		move_and_slide(velocity * G_SPD)
--- Paper_Town/Player.gd
+++ Paper_Town/Player.gd
@@ -1,19 +1,35 @@
 extends KinematicBody2D
 
-
-const P_SPEED = 200
-const T_SPEED = 250
-const JUMP_POWER = -400
+# platform move spped
+const P_SPD = 200
+# topdown move speed
+const T_SPD = 250
+# move speed when grabbing a platform
+const G_SPD = 120
+const JUMP_PWR = -400
 const GRAV = 15
 const PLATFORM = Vector2(0, -1)
 const TOPDOWN = Vector2(0, 0)
 
-var is_platform = false
+var isPlatform = false
 var velocity = Vector2()
+var isGrabbed = false
+
+onready var grabDetect = $GrabDetect
 
 
 func _ready():
 	pass # Replace with function body.
+
+func _physics_process(delta):
+	get_input()
+	if isPlatform:
+		velocity = move_and_slide(velocity, PLATFORM)
+	else:
+		if !isGrabbed:
+			move_and_slide(velocity * T_SPD, TOPDOWN)
+		else:
+			move_and_slide(velocity * G_SPD, TOPDOWN)
 
 func get_input():
 	var _right = Input.is_action_pressed("move_right")
@@ -27,27 +43,36 @@ func get_input():
 	var _switch = Input.is_action_just_pressed("switch")
 	
 	
-	if is_platform:
+	if isPlatform:
 		if _switch:
-			is_platform = false
+			isPlatform = false
 	else:
 		if _switch:
-			is_platform = true
+			isPlatform = true
 	
-	if is_platform:
-		velocity.x = (int(_right) - int(_left)) * P_SPEED
+	if isPlatform:
+		velocity.x = (int(_right) - int(_left)) * P_SPD
 		if is_on_floor() and _jump:
-			velocity.y = JUMP_POWER
+			velocity.y = JUMP_PWR
 		else:
 			velocity.y += GRAV
 	else:
 		velocity.x = int(_right) - int(_left)
 		velocity.y = int(_down) - int(_up)
-		velocity = velocity.normalized() * T_SPEED
+		velocity = velocity.normalized()
+	
+	var collider = grabDetect.get_collider()
+	
+	if _grab and !isGrabbed and !isPlatform:
+		if collider != null and collider.has_method("grabbed"):
+			collider.grabbed(true)
+			isGrabbed = true
 
-func _physics_process(delta):
-	get_input()
-	if is_platform:
-		velocity = move_and_slide(velocity, PLATFORM)
-	else:
-		move_and_slide(velocity, TOPDOWN)
+	if !_grab and isGrabbed:
+		if collider != null and collider.has_method("grabbed"):
+			collider.grabbed(false)
+			isGrabbed = false
+
+	if isGrabbed:
+		if collider != null and collider.has_method("get_motion"):
+			collider.get_motion(velocity)
