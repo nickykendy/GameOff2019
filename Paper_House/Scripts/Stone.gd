f7dcd688041961f2160662bdfb8ca1a148c9bbbb
NickyKendy Hua <nicky_kendy@hotmail.com>
2021-08-26 13:22:38 +0800

    update 2.01

--- Paper_House/Scripts/Stone.gd
+++ Paper_House/Scripts/Stone.gd
@@ -1,12 +1,15 @@
 extends StaticBody2D
 
-onready var spr = $AnimatedSprite
-onready var T_col = $T_CollisionShape2D
+onready var spr : = $AnimatedSprite
+onready var T_col : = $T_CollisionShape2D
+onready var P_col : = $P_CollisionShape2D
 
-func change(newGenre):
+func change(newGenre : bool) -> void:
 	if newGenre: # platform
 		spr.play("default", false)
+		P_col.disabled = false
 		T_col.disabled = true
 	else: # topdown
 		spr.play("default", true)
+		P_col.disabled = true
 		T_col.disabled = false
